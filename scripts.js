57da90cf9e0f9dd0e99ef1d7fc00ed24bf44a289
Matt Born <mattborn@users.noreply.github.com>
2023-05-17 18:45:09 -0600

    Start UI

--- scripts.js
+++ scripts.js
@@ -3,7 +3,9 @@ const q = document.querySelectorAll.bind(document)
 
 dayjs.extend(dayjs_plugin_relativeTime)
 
-fetch('/logs.json')
+const url = location.host.startsWith('l') ? '' : '/sagan'
+
+fetch(`${url}/logs.json`)
   .then(response => response.json())
   .then(data =>
     data.reverse().forEach(l => {
@@ -36,3 +38,147 @@ fetch('/logs.json')
       g('logs').appendChild(log)
     }),
   )
+
+g('logs-header').addEventListener('click', e => g('logs').remove())
+
+let state
+
+fetch(`${url}/data.json`)
+  .then(response => response.json())
+  .then(data => {
+    state = data
+    renderCities(data)
+  })
+
+const renderCities = data => {
+  g('cards').innerHTML = ''
+  resetBreadcrumbs()
+  data.cities.forEach(city => {
+    const card = document.createElement('div')
+    card.className = 'card'
+    card.addEventListener('click', e => renderAreas(city))
+
+    const h2 = document.createElement('h2')
+    h2.className = 'card-name'
+    h2.textContent = city.name
+    card.appendChild(h2)
+
+    const desc = document.createElement('p')
+    desc.className = 'card-desc'
+    desc.textContent = city.description
+    card.appendChild(desc)
+
+    const langs = document.createElement('div')
+    langs.className = 'card-langs'
+    card.appendChild(langs)
+    city.language.split(', ').forEach(l => {
+      const lang = document.createElement('div')
+      lang.className = 'card-lang'
+      lang.textContent = l
+      langs.appendChild(lang)
+    })
+
+    g('cards').appendChild(card)
+    animateCards()
+  })
+}
+
+const resetBreadcrumbs = () => {
+  const breadcrumb = document.createElement('div')
+  breadcrumb.className = 'breadcrumb'
+  breadcrumb.textContent = 'Cities'
+  breadcrumb.addEventListener('click', e => renderCities(state))
+  g('breadcrumbs').innerHTML = ''
+  g('breadcrumbs').appendChild(breadcrumb)
+}
+
+const animateCards = () => {
+  ScrollReveal().reveal('.card', {
+    cleanup: true,
+    distance: '20%',
+    interval: 50,
+    origin: 'bottom',
+  })
+}
+
+const renderAreas = city => {
+  g('cards').innerHTML = ''
+
+  const breadcrumb = document.createElement('div')
+  breadcrumb.className = 'breadcrumb'
+  breadcrumb.textContent = city.name
+  g('breadcrumbs').appendChild(breadcrumb)
+
+  city.areas.forEach(area => {
+    const card = document.createElement('div')
+    card.className = 'card'
+    card.addEventListener('click', e => renderPlans(area))
+
+    const h2 = document.createElement('h2')
+    h2.className = 'card-name'
+    h2.textContent = area.name
+    card.appendChild(h2)
+
+    const desc = document.createElement('p')
+    desc.className = 'card-desc'
+    desc.textContent = area.description
+    card.appendChild(desc)
+
+    g('cards').appendChild(card)
+    animateCards()
+  })
+}
+
+const renderPlans = area => {
+  g('cards').innerHTML = ''
+
+  const breadcrumb = document.createElement('div')
+  breadcrumb.className = 'breadcrumb'
+  breadcrumb.textContent = area.name
+  g('breadcrumbs').appendChild(breadcrumb)
+
+  area.plans.length
+    ? area.plans.forEach(plan => {
+        console.log(plan)
+        const card = document.createElement('div')
+        card.className = 'card plan'
+        card.addEventListener('click', e => renderPlan(plan))
+
+        const h2 = document.createElement('h2')
+        h2.className = 'card-name'
+        h2.textContent = plan.title
+        card.appendChild(h2)
+
+        let places = []
+        plan.parts.forEach(part => part.places.map(place => places.push(place.name)))
+
+        const desc = document.createElement('p')
+        desc.className = 'card-desc'
+        desc.textContent = places.join(' / ')
+        card.appendChild(desc)
+
+        g('cards').appendChild(card)
+        animateCards()
+      })
+    : renderEmpty()
+}
+
+const renderEmpty = () => {
+  g('cards').innerHTML = '<div>Be the first to create a plan for this area! <button>Generate</button></div>'
+}
+
+const renderPlan = () => {
+  const stage = document.createElement('div')
+  stage.id = 'stage'
+  stage.addEventListener('click', e => {
+    document.body.style.overflow = 'unset'
+    stage.remove()
+  })
+  document.body.appendChild(stage)
+  document.body.style.overflow = 'hidden'
+
+  const frame = document.createElement('div')
+  frame.className = 'iphone-14'
+  frame.textContent = 'slideshow goes here'
+  stage.appendChild(frame)
+}
